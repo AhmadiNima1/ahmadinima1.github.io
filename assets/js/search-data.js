cd19d5bc47aaaf3137e66cead174d0e010c1b7ad
AhmadiNima1 <40477591+AhmadiNima1@users.noreply.github.com>
2026-05-26 06:31:03 +0000

    Deploying to gh-pages from @ AhmadiNima1/ahmadinima1.github.io@8a9e6de07f4443e4765d187a5ad6d07d33522c8d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "Publications",
-          description: "Selected publications and conference proceedings.",
+          description: "Published work and manuscripts under review.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
